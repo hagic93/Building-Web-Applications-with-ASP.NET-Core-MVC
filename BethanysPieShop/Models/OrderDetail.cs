0e96b7725071e273739daad1d81a187754dc1077
Haris Agic <harisagicmo993@gmail.com>
2022-03-17 16:42:51 +0100

    Building Web Applications with ASP.NET Core MVC

---
+++ BethanysPieShop/Models/OrderDetail.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BethanysPieShop.Models
+{
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+        public int PieId { get; set; }
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+        [ForeignKey("PieId")]
+        public Pie Pie { get; set; }
+        [ForeignKey("OrderId")]
+        public Order Order { get; set; }
+    }
+}
---
+++ BethanysPieShop/Models/OrderRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly AppDbContext _appDbContext;
+        private readonly ShoppingCart _shoppingCart;
+
+
+        public OrderRepository(AppDbContext appDbContext, ShoppingCart shoppingCart)
+        {
+            _appDbContext = appDbContext;
+            _shoppingCart = shoppingCart;
+        }
+
+
+        public void CreateOrder(Order order)
+        {
+            try
+            {
+                order.OrderPlaced = DateTime.Now;
+
+              var Order =  _appDbContext.Orders.Add(order).Entity;
+                _appDbContext.SaveChanges();
+
+
+                var shoppingCartItems = _shoppingCart.ShoppingCartItems;
+
+                foreach (var shoppingCartItem in shoppingCartItems)
+                {
+                    var orderDetail = new OrderDetail()
+                    {
+                        Amount = shoppingCartItem.Amount,
+                        PieId = shoppingCartItem.Pie.PieId,
+                        OrderId = Order.OrderId,
+                        Price = shoppingCartItem.Pie.Price
+                    };
+
+                    _appDbContext.OrderDetails.Add(orderDetail);
+                }
+
+                _appDbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
